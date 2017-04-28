24349fca2edb646a81f974c4c882f2b10bdadb28
Gabriel Brasileiro <gah.lucas@gmail.com>
2017-04-28 10:38:04 -0300

    Add files via upload

---
+++ Projeto01(Introdução)/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+		
+	int a,b,c;
+	
+	a=10;
+	b=20;
+	c= a+b;
+	
+	printf("%d\n", c);
+	system("pause");
+	
+	return 0;
+	
+	
+}
