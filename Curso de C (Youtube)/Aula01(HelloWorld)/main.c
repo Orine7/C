d3f38c4ddc3c7408fdf14ae59eaba696ad1d039f
Gabriel Brasileiro <gah.lucas@gmail.com>
2017-06-08 03:08:18 -0300

    Add files via upload

---
+++ Curso de C (Youtube)/Aula01(HelloWorld)/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	// O "\n" representa a quebra de linha
+	
+	printf("Hello World! \n\n");
+	
+	system("pause");
+	return 0;
+}
