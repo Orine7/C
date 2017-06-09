8cb65fa14d799e27409ac3f1002448a02ccbbfc7
Gabriel Brasileiro <gah.lucas@gmail.com>
2017-06-09 08:47:47 -0300

    Add files via upload

---
+++ Curso de C (Youtube)/Aula09(Laço)/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i, j;
+	
+	for(i=0; i<10; i++){
+		
+		printf("Linha %d\n", i ); // Adicionando o mais um ao lado do i apresentará o valor em ordem + 1
+		
+		for(j=0; j<5; j++){
+			
+			printf("Teste  %d \n", j); 
+			
+		}
+		
+	}
+	
+}
