744b66c960ed64ef3b2b9700573f7116b109913b
Gabriel Brasileiro <gah.lucas@gmail.com>
2017-06-08 21:16:58 -0300

    Add files via upload

---
+++ Curso de C (Youtube)/Aula04(Operadores)/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	printf("%d\n", 5 + 2);  // 7
+	printf("%d\n", 9 - 4);  // 5
+	printf("%d\n", 2 * 3);  // 6
+	printf("%d\n", 10 / 5); // 2
+	printf("%d\n", 9 % 8);  // 1
+	
+	system("pause");
+	return 0;
+}
